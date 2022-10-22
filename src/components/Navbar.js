d59fcb09ef3654b4b5089faad9f809db4b4953dc
andrewsdeve <andrewszaratema@gmail.com>
2022-10-21 22:47:09 -0500

    second_deploy_navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Container, LogoContainer, Menu, MenuHamburg, MenuItem, MenuItemLink, Wrapper } from './Navbar.elements'
+import { FaBattleNet   } from "react-icons/fa";
+import { FaShoppingCart,FaBars} from 'react-icons/fa';
+import { IconContext } from 'react-icons';
+import '../index.css';
 
 const Navbar = () => {
+  const  [showMobileMenu, setShowMobileMenu] = useState(false);
+
+
   return (
-    <div>Navbar</div>
+    <Container>
+      <Wrapper>
+          <IconContext.Provider value ={{ style: {fontSize: "2em"}  }}>
+        <LogoContainer>
+          <FaBattleNet/>
+          <p>Shoes- </p>
+          <p> Online</p>
+        </LogoContainer>
+        <MenuHamburg onClick={() => setShowMobileMenu(! showMobileMenu) } >
+          <FaBars/>
+        </MenuHamburg>
+         <Menu open = {showMobileMenu}>
+          <MenuItem>
+            <MenuItemLink>
+              HOME
+            </MenuItemLink>
+          </MenuItem>
+          <MenuItem>
+            <MenuItemLink>
+              ABOUT ME
+            </MenuItemLink>
+          </MenuItem>
+          <MenuItem>
+            <MenuItemLink>
+              MEN
+            </MenuItemLink>
+          </MenuItem>
+          <MenuItem>
+            <MenuItemLink>
+              WOMEN
+            </MenuItemLink>
+          </MenuItem>
+          <MenuItem>
+            <MenuItemLink>
+              <FaShoppingCart/>
+            </MenuItemLink>
+          </MenuItem>
+        </Menu>
+        </IconContext.Provider>
+      </Wrapper>
+    </Container>
   )
 }
 
